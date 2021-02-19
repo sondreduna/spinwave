eb46787b5e21efbc62bfbf437484658090621300
Sondre Duna Lundemo <sondre@duna.no>
2021-02-19 22:14:52 +0100

    Made simple odesolver class
    Heun (& RK4 for comparison)

---
+++ src/ode.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class ODEStep:
+
+    def __init__(self, f, h, y0, t0 = 0):
+        self.f, self.h = f, h
+        self.t = t0
+        self.y = y0
+
+    def __call__(self):
+        """
+        Function for stepping forward
+        one unit of h in time
+
+        """
+        raise NotImplementedError
+
+class HeunStep(ODEStep):
+
+    def __call__(self):
+        t,y,h = self.t, self.y, self.h
+        
+        y_p = y + h   * self.f(t, y)
+        y_  = y + h/2 * (self.f(t,y) + self.f(t + h, y_p))
+
+        self.y = y_
+        self.t = t + h
+        return self.y
+
+class RK4Step(ODEStep):
+
+    def __call__(self):
+        t,y,h = self.t, self.y, self.h
+
+        k1  = self.f( t      , y)
+        k2  = self.f( t + h/2, y + k1 * h/2)
+        k3  = self.f( t + h/2, y + k2 * h/2)
+        k4  = self.f( t + h  , y + k3 * h)
+    
+        self.y =  y + h * (k1 + 2*k2 + 2*k3 + k4)/6
+        self.t += h
+        return self.y
+
+class ODESolver:
+
+    def __init__(self,f,t0,y0,tN,h,method = "Heun"):
+
+        if method == "Heun":
+            self.step = HeunStep(f,h,y0,t0)
+        elif method == "RK4":
+            self.step = RK4Step(f,h,y0,t0)
+        else:
+            raise NotImplementedError
+
+        self.tN = tN 
+        self.N  = int((tN - t0)/h)
+
+        self.dim = np.size(y0)
+        self.Y = np.zeros((self.N + 1, self.dim))
+        self.T = np.arange(t0, t0 + (self.N + 1) * h, h)
+
+    def __call__(self):
+        self.Y[0] = self.step.y
+
+        for i in range(1,self.N + 1):
+            self.step.h = min(self.step.h, self.tN - self.T[i-1])
+            self.Y[i] = self.step()
+
+        return self.T, self.Y
+        
---
+++ src/tests.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from time import time
+
+rc("text",usetex = True)
+rc("font",family = "sans-serif")
+
+fontsize = 25
+newparams = {'axes.titlesize': fontsize,
+             'axes.labelsize': fontsize,
+             'ytick.labelsize': fontsize,
+             'xtick.labelsize': fontsize, 
+             'legend.fontsize': fontsize,
+             'figure.titlesize': fontsize,
+             'legend.handlelength': 1.5, 
+             'lines.linewidth': 2,
+             'lines.markersize': 7,
+             'figure.figsize': (11, 7), 
+             'figure.dpi':200,
+             'text.usetex' : True,
+             'font.family' : "sans-serif"
+            }
+
+plt.rcParams.update(newparams)
+
+from ode import *
+
+def fa(t, X):
+    # Example taken from beginning of Chapter II in
+    # Hairer, Nørsett, Wanner
+    # Solving Ordinary Differential Equations, vol I
+    # X[0] = y1
+    # X[1] = y1dot
+    # X[2] = y2
+    # X[3] = y2dot
+    # Constants
+    mu  = 0.012277471
+    mu_ = 1 - mu
+    # Variable parameters
+    D1 = ((X[0] + mu )**2 + X[2]**2)**(3/2)
+    D2 = ((X[0] - mu_)**2 + X[2]**2)**(3/2)
+    # Derivatives
+    dX0 = X[1]
+    dX1 = X[0] + 2*X[3] - mu_*(X[0] + mu)/D1 - mu*(X[0] - mu_)/D2
+    dX2 = X[3]
+    dX3 = X[2] - 2*X[1] - mu_*X[2]/D1        - mu*X[2]/D2
+    return np.array([dX0, dX1, dX2, dX3])
+
+def ode_solver_test():
+
+    t  = 0
+    T  = 17.06521656015796
+    X0 = np.array([
+         0.994, # y1(0)
+         0.0,   # y1'(0)
+         0.0,   # y2(0)
+        -2.001585106379082522405 # y2'(0)
+        ])
+
+
+    # Intergrate for a duration equal to 2T.
+    # If the error after the first orbit is too large,
+    # the second orbit will be completely off.
+    # Try experimenting with the timestep.
+    h  = 5e-4
+
+    fasolver = ODESolver(fa,0,X0,2*T,h,"RK4")
+    # Also, measure the time of the calculation
+    tic = time()
+    Ts, Xs = fasolver()
+    toc = time()
+    print('Number of steps: ', len(Ts))
+    print('Simulation took ', toc - tic, ' seconds')
+
+    fig = plt.figure(figsize = (12, 6))
+    plt.plot(Xs[:,0], Xs[:,2])
+    plt.tight_layout()
+    fig.savefig("../fig/odetest.pdf")
+    
